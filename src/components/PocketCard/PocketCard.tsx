ecd4b9c8260fd27c59b1f9857ebd9057e4df180d
Yury A Troynov <yuriy.troynov@brainly.com>
2019-09-24 16:09:11 +0200

    add pocket card

---
+++ src/components/PocketCard/PocketCard.tsx
@@ -0,0 +1,48 @@
+import React, { ReactElement } from 'react';
+import {
+  createStyles,
+  Grid,
+  makeStyles,
+  Paper,
+  Theme
+} from '@material-ui/core';
+import { CurrencyType } from '../../types/CurrencyType';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWallet } from '@fortawesome/free-solid-svg-icons';
+
+type PocketCardPropsType = Readonly<{
+  currency: CurrencyType;
+  amount: number;
+}>;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    contentContainer: {
+      padding: theme.spacing(2),
+      margin: theme.spacing(2)
+    }
+  })
+);
+
+export const PocketCard = ({
+  currency,
+  amount
+}: PocketCardPropsType): ReactElement => {
+  const classes = useStyles();
+
+  return (
+    <Paper className={classes.contentContainer}>
+      <Grid container justify="space-between">
+        <Grid item xs={2}>
+          <Grid container alignItems="center" justify="space-between">
+            <FontAwesomeIcon icon={faWallet} size="2x" />
+            <Grid item>{currency}</Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={1}>
+          {amount}
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
