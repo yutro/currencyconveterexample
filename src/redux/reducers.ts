8ef8879cd7293bc497e0807968c8cfe3d30b67ea
Yury A Troynov <yuriy.troynov@brainly.com>
2019-09-24 16:11:45 +0200

    reducers

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
+import { PocketStateType} from '../pages/Pocket/PocketTypes';
+import { pockets } from '../pages/Pocket/pocketReducer';
+import { exchange } from '../pages/Exchange/exchangeReducer';
+import { ExchangeType } from '../pages/Exchange/exchangeTypes';
 
-const initialState = {};
-const app = (state = initialState) => {
-  return state;
+export type AppStateType = {
+  pockets: PocketStateType;
+  exchange: ExchangeType;
 };
 
-export default combineReducers({ app });
+export default combineReducers({ pockets, exchange });
