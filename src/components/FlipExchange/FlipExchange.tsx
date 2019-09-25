45145ccdaaf67ac0bd988f122ba8911342c6b6d5
Yury A Troynov <yuriy.troynov@brainly.com>
2019-09-25 22:31:12 +0200

    flip exchange styling

--- src/components/FlipExchange/FlipExchange.tsx
+++ src/components/FlipExchange/FlipExchange.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { useDispatch } from 'react-redux';
 import { Fab } from '@material-ui/core';
 import { Cached as IconCached } from '@material-ui/icons';
 
-export const FlipExchange = () => {
+type PropsType = Readonly<{
+  className?: string;
+}>;
+
+export const FlipExchange = (props: PropsType): ReactNode => {
   const dispatch = useDispatch();
 
   return (
     <Fab
+      {...props}
       color="primary"
       size="small"
       aria-label="flip-exchange"
--- src/pages/Exchange/Exchange.tsx
+++ src/pages/Exchange/Exchange.tsx
@@ -4,7 +4,7 @@ import { AppStateType } from '../../redux/reducers';
 import { ConvertButton } from '../../components/ConvertButton/ConvertButton';
 import { CurrencyPrompt } from '../../components/CurrencyPrompt/CurrencyPrompt';
 import { useDispatch, useSelector } from 'react-redux';
-import { Grid } from '@material-ui/core';
+import { createStyles, Grid, makeStyles, Theme } from '@material-ui/core';
 import { PocketType } from '../Pocket/PocketTypes';
 import { fxData } from '../../tests/mocks/FxApiResponse';
 import { FlipExchange } from '../../components/FlipExchange/FlipExchange';
@@ -17,7 +17,16 @@ const excludeCurrency = (
     (currency: CurrencyType): boolean => currency !== excludeCurrency
   );
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    flipExchange: {
+      marginLeft: theme.spacing(5)
+    }
+  })
+);
+
 export const Exchange: FunctionComponent = () => {
+  const classes = useStyles();
   const pockets = useSelector(({ pockets }: AppStateType) => pockets);
   const { currencies } = useSelector(({ exchange }: AppStateType) => exchange);
   const dispatch = useDispatch();
@@ -26,6 +35,7 @@ export const Exchange: FunctionComponent = () => {
 
     return currency;
   });
+  const [exchangeCardBase, exchangeCardTarget] = currencies;
 
   useEffect(() => {
     const intervalID: any = setInterval(() => {
@@ -49,32 +59,23 @@ export const Exchange: FunctionComponent = () => {
     return (): void => clearInterval(intervalID);
   }, [dispatch]);
 
-  const [exchangeCardBase, exchangeCardTarget] = currencies;
-
   return (
     <>
+      <CurrencyPrompt
+        {...exchangeCardBase}
+        index={0}
+        currencyList={excludeCurrency(
+          currencyList,
+          exchangeCardTarget.currency
+        )}
+      />
+      <FlipExchange className={classes.flipExchange} />
+      <CurrencyPrompt
+        {...exchangeCardTarget}
+        index={1}
+        currencyList={excludeCurrency(currencyList, exchangeCardBase.currency)}
+      />
       <Grid container justify="center">
-        <Grid item>
-          <CurrencyPrompt
-            {...exchangeCardBase}
-            index={0}
-            currencyList={excludeCurrency(
-              currencyList,
-              exchangeCardTarget.currency
-            )}
-          />
-        </Grid>
-        <FlipExchange />
-        <Grid item>
-          <CurrencyPrompt
-            {...exchangeCardTarget}
-            index={1}
-            currencyList={excludeCurrency(
-              currencyList,
-              exchangeCardBase.currency
-            )}
-          />
-        </Grid>
         <ConvertButton />
       </Grid>
     </>
