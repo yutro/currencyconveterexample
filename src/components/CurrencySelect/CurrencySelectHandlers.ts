116711c607edd16a6df3a835668ec8ff3b692d8b
Yury A Troynov <yuriy.troynov@brainly.com>
2019-09-26 21:19:45 +0200

    currency select types

--- src/components/CurrencySelect/CurrencySelectHandlers.ts
+++ src/components/CurrencySelect/CurrencySelectHandlers.ts
@@ -1,9 +1,10 @@
 import { ChangeEvent } from 'react';
 import { CurrencyType } from '../../types/CurrencyType';
+import { SelectHandlerType } from './CurrencySelect';
 
 export const selectHandler = (onSelect: SelectHandlerType) => ({
   target: { value }
-}: ChangeEvent<{ value: unknown }>) => {
+}: ChangeEvent<{ value: unknown }>): void => {
   const val = value as CurrencyType;
 
   if (val) {
