bce227a25b159042665c3ed545d230e85e78563a
Yury A Troynov <yuriy.troynov@brainly.com>
2019-09-27 15:41:03 +0200

    rename Layout file to AppLayout

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import { store } from './redux';
-import { AppLayout } from './components/AppLayout/Layout';
+import { AppLayout } from './components/AppLayout/AppLayout';
 
 ReactDOM.render(
   <Provider store={store}>
