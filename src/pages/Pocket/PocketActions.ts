e0b380b5fc37e04c277a3dcf977118b5934de4a7
Yury A Troynov <yuriy.troynov@brainly.com>
2019-09-28 12:52:50 +0200

    pocket actions typed

--- src/pages/Pocket/PocketActions.ts
+++ src/pages/Pocket/PocketActions.ts
@@ -1,14 +1,27 @@
 import { CurrencyType } from '../../types/CurrencyType';
-import { getReturnType } from '../../types/utils';
+
+const UPDATE_BASE_POCKET = 'UPDATE_BASE_POCKET';
+const UPDATE_TARGET_POCKET = 'UPDATE_TARGET_POCKET';
+
+type PayloadType = { amount: number; currency: CurrencyType };
+
+type UpdateBasePocketReturnType = {
+  type: typeof UPDATE_BASE_POCKET;
+  payload: PayloadType;
+};
+type UpdateTargetPocketReturnType = {
+  type: typeof UPDATE_TARGET_POCKET;
+  payload: PayloadType;
+};
 
 export const updatePocket = (
   type: 'base' | 'target',
-  payload: { amount: number; currency: CurrencyType }
-) => ({
+  payload: PayloadType
+): UpdateBasePocketReturnType | UpdateTargetPocketReturnType => ({
   payload,
-  type: type === 'base' ? 'UPDATE_BASE_POCKET' : 'UPDATE_TARGET_POCKET'
+  type: type === 'base' ? UPDATE_BASE_POCKET : UPDATE_TARGET_POCKET
 });
 
-const UpdatePocketReturnType = getReturnType(updatePocket);
-
-export type PocketActions = typeof UpdatePocketReturnType;
+export type PocketActions =
+  | UpdateBasePocketReturnType
+  | UpdateTargetPocketReturnType;
