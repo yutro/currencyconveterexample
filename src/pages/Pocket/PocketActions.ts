653da1707faacf57cd691f02b5b67bf538f2b62d
Yury A Troynov <yuriy.troynov@brainly.com>
2019-09-28 13:02:51 +0200

    pocket actions refactoring

--- src/pages/Pocket/PocketActions.ts
+++ src/pages/Pocket/PocketActions.ts
@@ -5,23 +5,17 @@ const UPDATE_TARGET_POCKET = 'UPDATE_TARGET_POCKET';
 
 type PayloadType = { amount: number; currency: CurrencyType };
 
-type UpdateBasePocketReturnType = {
-  type: typeof UPDATE_BASE_POCKET;
-  payload: PayloadType;
-};
-type UpdateTargetPocketReturnType = {
-  type: typeof UPDATE_TARGET_POCKET;
+type UpdatePocketReturnType = {
+  type: typeof UPDATE_BASE_POCKET | typeof UPDATE_TARGET_POCKET;
   payload: PayloadType;
 };
 
 export const updatePocket = (
   type: 'base' | 'target',
   payload: PayloadType
-): UpdateBasePocketReturnType | UpdateTargetPocketReturnType => ({
+): UpdatePocketReturnType => ({
   payload,
   type: type === 'base' ? UPDATE_BASE_POCKET : UPDATE_TARGET_POCKET
 });
 
-export type PocketActions =
-  | UpdateBasePocketReturnType
-  | UpdateTargetPocketReturnType;
+export type PocketActions = UpdatePocketReturnType;
