727a9d9c5a7dcf696b5b2085671275e3eca48d44
Yury A Troynov <yuriy.troynov@brainly.com>
2019-10-03 00:03:36 +0200

    adjust e2e tests for new api mock values

--- src/pages/Exchange/Exchange_test.js
+++ src/pages/Exchange/Exchange_test.js
@@ -12,7 +12,7 @@ Scenario('should exchange value from base card to target', async I => {
   I.fillField('amount-0', '10');
   const val = await I.grabValueFrom('amount-1');
 
-  assert.strictEqual(val, '8.1', 'target card amount not eql');
+  assert.strictEqual(val, '7.98', 'target card amount not eql');
 });
 
 Scenario(
@@ -27,8 +27,8 @@ Scenario(
     const baseInputVal = await I.grabValueFrom('amount-0');
     const targetInputVal = await I.grabValueFrom('amount-1');
 
-    assert.strictEqual(baseInputVal, '8.1');
-    assert.strictEqual(targetInputVal, '10.04');
+    assert.strictEqual(baseInputVal, '7.98');
+    assert.strictEqual(targetInputVal, '10');
   }
 );
 
@@ -40,7 +40,7 @@ Scenario('recalculate target value after target currency changed', async I => {
   // target value before change target currency
   const targetInputValBeforeUpdate = await I.grabValueFrom('amount-1');
 
-  assert.strictEqual(targetInputValBeforeUpdate, '8.1');
+  assert.strictEqual(targetInputValBeforeUpdate, '7.98');
 
   I.click('//*[@id="select-prompt-1"]');
   I.wait(1);
@@ -48,5 +48,5 @@ Scenario('recalculate target value after target currency changed', async I => {
   // target value after change target currency
   const targetInputValAfterUpdate = await I.grabValueFrom('amount-1');
 
-  assert.strictEqual(targetInputValAfterUpdate, '9.1');
+  assert.strictEqual(targetInputValAfterUpdate, '12.53');
 });
