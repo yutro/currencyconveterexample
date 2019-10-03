a7d0b6160f86a5d9d86af6cd867247f294ef09e0
Yury A Troynov <yuriy.troynov@brainly.com>
2019-10-03 11:01:36 +0200

    remove redundant

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,18 +1,6 @@
 import { CurrencyType } from '../types/CurrencyType';
 import { ExchangeType } from '../pages/Exchange/exchangeTypes';
 
-export const keysOf = <T extends string>(
-  obj: { [key in T]: unknown }
-): ReadonlyArray<T> => {
-  const keys: Array<T> = [];
-
-  for (const i in obj) {
-    keys.push(i);
-  }
-
-  return keys;
-};
-
 export const convertThroughCrossCourse = (
   baseRate: number,
   targetRate: number,
