5ebe72fb7a738a7d27fe08192704d47dd70e72a1
Yury A Troynov <yuriy.troynov@brainly.com>
2019-10-03 12:16:36 +0200

    redux dev tools typed

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,8 +1,15 @@
 import { createStore } from 'redux';
 import reducers from './reducers';
 
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION__: Function;
+  }
+}
+
 export const store = createStore(
   reducers,
-    // @ts-ignore
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : null
 );
